e07e2978730e59490d3eca6b9fbb4d41791d6227
yayumi <yayumi@gmail.com>
2024-11-01 19:16:09 +0900

    Sync up6

--- Auto_stop.py
+++ Auto_stop.py
@@ -51,9 +51,9 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-password = '45!1!2'
+#password = '45!1!2'
 db_pass='drr1'
-print 'ss'
+#print 'ss'
 print 'ss'
 
 def ec2_rds_stop(regions_list):
